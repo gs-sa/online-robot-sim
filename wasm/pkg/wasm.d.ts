8619740dc17d6de5fcfb08cf3d38416c5bf8b4b0
gesheng <12132253@mail.sustech.edu.cn>
2023-08-15 21:49:35 +0800

    add wasm

---
+++ wasm/pkg/wasm.d.ts
@@ -0,0 +1,52 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export class Robot {
+  free(): void;
+/**
+* @param {string} urdf
+* @param {string} tip
+*/
+  constructor(urdf: string, tip: string);
+/**
+* @param {Float64Array} target
+* @param {Float64Array} init
+* @returns {Float64Array}
+*/
+  ik(target: Float64Array, init: Float64Array): Float64Array;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_robot_free: (a: number) => void;
+  readonly robot_new: (a: number, b: number, c: number, d: number) => number;
+  readonly robot_ik: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
